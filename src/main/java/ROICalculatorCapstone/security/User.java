e9e0f7e755adf4c203f0f58fffd3a5f727697a4f
paulrod3 <Paul_rodriguez3@yahoo.com>
2023-07-12 15:39:00 -0500

    added Web Sec Config, User, User Rep, UserServiceImpl classes. Everything is working so far.

--- src/main/java/ROICalculatorCapstone/security/User.java
+++ src/main/java/ROICalculatorCapstone/security/User.java
@@ -1,6 +1,69 @@
 package ROICalculatorCapstone.security;
 
+import javax.management.relation.Role;
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+    public enum Role {
+        USER,
+        ADMIN
+    }
+    // Constructors, getters, and setters
+
+
+    public User(Long id, String username, String password, Set<Role> roles) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
---
+++ src/main/java/ROICalculatorCapstone/security/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package ROICalculatorCapstone.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.SecurityConfig;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("user")
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN");
+    }
+
+
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
